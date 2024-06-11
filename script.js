5158357c2cab98e3e0c99c192a2ae3892799e606
Guilherme Portela Meller <guilhermeportelameller@gmail.com>
2024-06-11 13:40:06 -0300

    Added_Style

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+document.getElementById('cep').addEventListener('input', function(event) {
+    this.value = this.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+});
+
 document.getElementById('searchForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const cep = document.getElementById('cep').value.trim(); // Remove espaços extras
     const numero = document.getElementById('numero').value.trim(); // Remove espaços extras
     const resultDiv = document.getElementById('result');
+    const modal = document.getElementById('resultModal');
+    const modalMessage = document.getElementById('modalMessage');
+
+    // Verifica se o CEP está vazio ou contém apenas espaços
+    if (!cep || cep.length === 0) {
+        modalMessage.innerHTML = 'Por favor, insira um CEP válido.';
+        modal.style.display = 'block';
+        return;
+    }
 
     // Limpar o conteúdo da div de resultado antes de iniciar uma nova busca
     resultDiv.innerHTML = '';
@@ -25,13 +38,32 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
             }
 
             if (found) {
-                resultDiv.innerHTML = '<p style="color: green;">Possui Viabilidade.</p>';
+                modalMessage.innerHTML = `<span style="color: green;">O CEP ${cep}, número ${numero} Possui Viabilidade ✔.</span>`;
             } else {
-                resultDiv.innerHTML = '<p style="color: red;">Não possui Viabilidade.</p>';
+                modalMessage.innerHTML = `<span style="color: red;">O CEP ${cep}, número ${numero} Não Possui Viabilidade ✖.</span>`;
             }
+            modal.style.display = 'block';
         },
         error: function(error) {
-            resultDiv.innerHTML = `<p style="color: red;">Erro ao processar o arquivo: ${error.message}</p>`;
+            modalMessage.innerHTML = `Erro ao processar o arquivo: ${error.message}`;
+            modal.style.display = 'block';
         }
     });
+});
+
+document.getElementById('clearButton').addEventListener('click', function() {
+    document.getElementById('cep').value = '';
+    document.getElementById('numero').value = '';
+    document.getElementById('result').innerHTML = '';
+});
+
+document.getElementById('okButton').addEventListener('click', function() {
+    document.getElementById('cep').value = '';
+    document.getElementById('numero').value = '';
+    document.getElementById('result').innerHTML = '';
+    document.getElementById('resultModal').style.display = 'none';
+});
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('resultModal').style.display = 'none';
 });
